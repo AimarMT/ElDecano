f3abad289cefdcfb28bc27f032562719258969ff
AimarMT <aimarmt14@gmail.com>
2026-03-05 16:16:51 +0100

    commit test1

---
+++ Assets/MyAssets/Scripts/MultijugadorTesting/NetworkedGrab.cs
@@ -0,0 +1,46 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+
+[RequireComponent(typeof(NetworkObject), typeof(UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable))]
+public class NetworkedGrab : NetworkBehaviour
+{
+    private NetworkObject networkObject;
+    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable interactable;
+
+    void Awake()
+    {
+        networkObject = GetComponent<NetworkObject>();
+        interactable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+        interactable.selectEntered.AddListener((args) => RequestOwnership());
+        interactable.selectExited.AddListener((args) => ReleaseOwnership());
+    }
+
+    private void RequestOwnership()
+    {
+        ulong clientID = NetworkManager.Singleton.LocalClientId;
+        RequestOwnershipRpc(clientID);
+    }
+
+    private void ReleaseOwnership()
+    {
+        ReleaseOwnershipRpc();
+    }
+
+
+    [Rpc(SendTo.Server)]
+    private void RequestOwnershipRpc(ulong clientID)
+    {
+        Debug.Log("changing ownership");
+        networkObject.ChangeOwnership(clientID);
+    }
+
+    [Rpc(SendTo.Server)]
+    private void ReleaseOwnershipRpc()
+    {
+        Debug.Log("releasing ownership");
+        networkObject.RemoveOwnership();
+    }
+
+
+}
